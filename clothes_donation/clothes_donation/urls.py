123082053451672737b027b9450c2ec68ce9908b
Shivam Kashyap <iamsantanihindu@gmail.com>
2026-05-23 14:31:54 +0530

    fix: explicitly serve media files in production mode

--- clothes_donation/clothes_donation/urls.py
+++ clothes_donation/clothes_donation/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
-from django.conf.urls.static import static
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -26,4 +26,9 @@ urlpatterns = [
     path('donors/', include('donors.urls')),
     path('receivers/', include('receivers.urls')),
     path('api/', include('api.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    
+    # Explicitly serve user-uploaded media files in production
+    re_path(r'^media/(?P<path>.*)$', serve, {
+        'document_root': settings.MEDIA_ROOT,
+    }),
+]
